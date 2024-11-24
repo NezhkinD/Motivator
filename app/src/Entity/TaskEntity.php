ea63101d3a4e3270fdb92dc984f3d7e45a7ffaa3
dnezhkin <dnezhkin@kuber.ru>
2024-11-24 22:03:59 +0300

    23-11-24

--- app/src/Entity/TaskEntity.php
+++ app/src/Entity/TaskEntity.php
@@ -14,15 +14,19 @@ class TaskEntity
     /** Начисленные баллы, исходя из result и коэффицентов */
     public float $score;
 
+    /** Описание задачи */
+    public ?string $description = null;
+
     /**
      * @throws \Exception
      */
-    public static function create(CategoryEnum $categoryEnum, int $result, int $score): self
+    public static function create(CategoryEnum $categoryEnum, int $result, int $score, ?string $description = null): self
     {
         $self = new self();
         $self->category = $categoryEnum;
         $self->result = $result;
         $self->score = $score;
+        $self->description = $description;
         return $self;
     }
 }
